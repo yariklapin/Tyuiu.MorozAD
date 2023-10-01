c6a6af47907d4fd728a34e910f2f9006741d70a4
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-10-01 17:07:17 +0500

    Task4

---
+++ Tyuiu.MorozAd.Sprint0.Task4.V0.lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MorozAd.Sprint0.Task4.V0.lib
+{
+    public class DataService
+    {
+        public static int Addition(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Subraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.MorozAd.Sprint0.Task4.V0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MorozAd.Sprint0.Task4.V0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MorozAd.Sprint0.Task4.V0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c253bc6c-4923-4ce1-b5eb-badf288a25ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
