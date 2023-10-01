60621cc966b9cd3814473d4fdbf90380bd56fcc1
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-10-01 16:16:43 +0500

    Task1

---
+++ Tyuiu.MorozAd.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MorozAd.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            string name ="Артём";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет ");
+            Console.ReadKey();
+
+        }
+    }
+}
